49427bb3ffd62243c275f79623096a7ef5205b1a
lisvega <lissyrivera456@gmail.com>
2022-07-12 18:58:31 +0200

    Iteración 5.4

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if (number1 === 10) {
 }
 
 
-
+// Iteración 5.1
 if (number2 / number1 == 2) {
     console.log("number2 dividido entre number1 es igual a 2");
 }
@@ -25,4 +25,8 @@ if (number1 !== number2) {
 
 if (number3 != number1) {
     console.log("number3 es distinto number1");
+}
+// Iteración 5.4
+if (number3 * 5 == number1) {
+    console.log("number3 por 5 es igual a number1");
 }
