71e10fc6b954b3e5a4331ef2ff16c43d2fa3a01b
lisvega <lissyrivera456@gmail.com>
2022-07-13 19:50:16 +0200

    Iteración # 1.2

--- app.js
+++ app.js
@@ -26,6 +26,25 @@ character.age = 25;
 console.log(character);
 
 
+/*1.2 Declara 3 variables con los nombres y valores siguientes 
+    firstName = 'Jon'; 
+    lastName = 'Snow'; 
+    age = 24; 
+Muestralos por consola de esta forma: 
+    'Soy Jon Snow, tengo 24 años y me gustan los lobos.' */
+
+let firstName = 'Jon';
+let lastName = 'Snow';
+let age = 24;
+
+console.log("Soy" + " " + firstName + " " + lastName + " , " + "tengo" + " " + age + " años y me gustan los lobos");
+
+// se puede hacer de las dos formas
+//console.log(`soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`);
+
+
+
+
 
 
 
