518978c6050301494d9150b6a1c2f1473734efde
lisvega <lissyrivera456@gmail.com>
2022-07-16 23:41:17 +0200

    Iteración #4, 1.1

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //--------------------------------------Iteración #1: Variables---------------------------------------------------------------------------------------------
 // 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 let myFavoriteHero = "Hulk";
@@ -16,7 +15,8 @@ let Y = "10";
 let z = "x + 5";
 
 
-//------------------------------Iteración #2: Variables avanzadas-----------------------------------------------------------------------------------
+//------------------------------Iteración #2: Variables avanzadas------------------------------------------------------
+
 
 /*1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 const character = {name: 'Jack Sparrow', age: 10};*/
@@ -40,9 +40,6 @@ console.log("Soy" + " " + firstName + " " + lastName + " , " + "tengo" + " " + a
 
 // se puede hacer de las dos formas
 //console.log(`soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`);
-
-
-
 
 
 
@@ -84,28 +81,20 @@ car2.finalPrice = car2.basePrice + globalBasePrice;
 console.log("MODIFICADO", car2);
 
 
-
-
-
-
-//--------------------------------------Iteración #3: Operadores--------------------------------------------------------------
+//--------------------------------------Iteración #3: Operadores---------------------------------
 
 //1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
 let Multiplicación = 10 * 5;
 console.log(Multiplicación);
-
 
 //1.2 Divide 10 por 2 y muestra el resultado en un console.
 let división = 10 / 2;
 console.log(división);
 
 
-
-
 //1.3 Muestra mediante un console el resto de dividir 15 por 9.
 let resto = 15 % 9;
 console.log(resto);
-
 
 /*1.4 Usa el correcto operador de asignación que resultará en o = 15, 
 teniendo dos variables
@@ -114,7 +103,6 @@ let p = 10;
 let j = 5;
 let o = p + j;
 console.log(p + j);
-
 
 
 /*1.5 Usa el correcto operador de asignación que resultará en i = 50,
@@ -128,113 +116,12 @@ console.log(c * m);
 
 
 
+
 //-------------------------------------**Iteración #4: Arrays**------------------------------------------
 
 //1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 const nameAvenger = avengers[0];
 console.log(nameAvenger);
-
-
-
-
-
-
-
-
-
-//-----------------------------------------------ejercicio 5 condicionales---------------------------------------------------
-
-//
-const number1 = 10;
-const number2 = 20;
-const number3 = 2;
-
-// ejemplo
-if (number1 === 10) {
-    console.log('number1 es estrictamente igual a 10')
-}
-
-
-// Iteración 5.1
-if (number2 / number1 == 2) {
-    console.log("number2 dividido entre number1 es igual a 2");
-}
-//(/* COMPLETAR */)
-
-
-
-//Iteracion 5.2
-if (number1 !== number2) {
-    console.log("number1 es estrictamente distinto a number2");
-}
-//(/* COMPLETAR */)
-
-
-// Iteracion 5.3
-if (number3 != number1) {
-    console.log("number3 es distinto number1");
-}
-//(/* COMPLETAR */)
-
-
-
-// Iteración 5.4
-if ((number3 * 5) == number1) {
-    console.log("number3 por 5 es igual a number1");
-}
-//(/* COMPLETAR */)
-
-
-// Iteración 5.5
-if (((number3 * 5) == number1) && ((number1 * 2) == number2)) {
-    console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
-}
-//(/* COMPLETAR */)
-
-
-// Iteracion 5.6
-if (((number2 / 2) == number1) || ((number1 / 5) == number3)) {
-    console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
-}
-//(/* COMPLETAR */)
-
-
-
-//----------------------------------- Ejercicio 6 Bucles------------------------------------------------------------
-
-//1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
-for (let p = 0; p < 10; p++) {
-    console.log(p)
-}
-
-
-
-//1.2  Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
-//cuando el resto del numero dividido entre 2 sea 0.
-
-for (let p = 0; p < 10; p++) {
-    if (p % 2 == 0) {
-        console.log(p)
-    }
-}
-
-
-/*1.3 Crea un bucle para conseguir dormir contando ovejas. 
-Este bucle empieza en 0 y termina en 10. 
-Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
-y cambia el mensaje en la última vuelta a 'Dormido!'.*/
-
-for (let p = 0; p < 10; p++) {
-    if (p < 9) {
-        console.log("intentando dormir");
-
-
-    }
-    else {
-        console.log("Dormido");
-    }
-
-}
 
 
